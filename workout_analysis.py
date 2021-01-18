37c6506eac6271419ec33d2fe5ddd4c32004e533
joshua-ruby <josh.r.ruby@gmail.com>
2021-01-18 00:33:32 -0800

    Improve json simplification logic

--- workout_analysis.py
+++ workout_analysis.py
@@ -8,6 +8,10 @@ import time
 import pandas as pd
 import fitdecode as fd
 
+'### Figure out how to handle the duplicate null speed and distance values from zwift rides and check if the same issue exists for rides uploaded from Wahoo Fitness'
+
+'### Need to look at `data_message`s with names in `[record, activity, event, lap, session]`. Figure out what info is in each one and which names are relevant for Zwift, Wahoo Fitness, and outdoor rides'
+
 def load_lottieurl(url: str):
     r = requests.get(url)
     if r.status_code != 200:
@@ -53,31 +57,45 @@ json_dict = dict_for_file_type(Path.cwd(), '.json')
 selected_json_dict_keys = st.multiselect('Select .json Files to Inspect', list(json_dict.keys()))
 
 if st.button('Inspect JSON(s)'):
-    simplified_json_ride_lists = {}
+    simplified_rides = {}
+
     for json_stem in selected_json_dict_keys:
         with open(json_dict[json_stem], 'r') as f:
             f_json = json.load(f)
+            st.write(json_stem, len(f_json))
 
-            simplified_json_ride_lists[json_stem] = []
+            simplified_rides[json_stem] = []
+
             for frame in f_json:
                 if frame['frame_type'] == 'data_message' and frame['name'] == 'record':
-                    payload = {
-                        'timestamp': frame['fields'][0],
-                        'position_lat': frame['fields'][1],
-                        'position_long': frame['fields'][2],
-                        'distance': frame['fields'][2],
-                        'heart_rate': frame['fields'][8],
-                        'altitude': frame['fields'][10],
-                        'speed': frame['fields'][12],
-                        'power': frame['fields'][13],
-                        'grade': frame['fields'][14],
-                        'cycle_length': frame['fields'][17],
-                        'temperature': frame['fields'][17],
-                        'chunk': frame['chunk'],
-                    }
-                    simplified_json_ride_lists[json_stem].append(payload)
+                    simplified_frame = {}
+                    stats_of_interest = [
+                        'timestamp',
+                        'position_lat',
+                        'position_long',
+                        'distance',
+                        'speed',
+                        'altitude',
+                        'ascent',
+                        'heart_rate',
+                        'power',
+                        'left_right_balance',
+                        'left_pedal_smoothness',
+                        'right_pedal_smoothness',
+                        'left_torque_effectiveness',
+                        'right_torque_effectiveness',
+                        'grade',
+                        'cadence',
+                        'temperature'
+                    ]
+                    for stat in stats_of_interest:
+                        simplified_frame[stat] = {}
+                        for field in frame['fields']:
+                            if field['name'] == stat:
+                                if field['value'] != None:
+                                    simplified_frame[stat]['value'] = field['value']
+                                    simplified_frame[stat]['units'] = field['units']
+                    simplified_frame['chunk'] = frame['chunk']
+                    simplified_rides[json_stem].append(simplified_frame)
+            st.write(simplified_rides[json_stem][:4])
 
-    for k, v in simplified_json_ride_lists.items():
-        st.write(k, len(v))
-        for itm in v[:4]:
-            st.write([itm['timestamp']['value'], itm['heart_rate']['value'], itm['power']['value']])
