45a71076950eb51b14dc26beaf4cb0a9fab30919
joshua-ruby <josh.r.ruby@gmail.com>
2021-01-17 19:47:12 -0800

    Import requests and display lottie demo

--- workout_analysis.py
+++ workout_analysis.py
@@ -1,5 +1,17 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
+import requests
 import pandas as pd
 import fitdecode as fd
 
-# Workout Analysis
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+    # Workout Analysis
+
+lottie_url = "https://assets5.lottiefiles.com/packages/lf20_V9t630.json"
+lottie_json = load_lottieurl(lottie_url)
+st_lottie(lottie_json)
