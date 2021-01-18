dba41f40461381e492a4000cb495bbd3e2a00d1d
joshua-ruby <josh.r.ruby@gmail.com>
2021-01-18 13:12:49 -0800

    Parse uploaded .fit into df

--- workout_analysis.py
+++ workout_analysis.py
@@ -31,7 +31,56 @@ def dict_for_file_type(directory, suffix):
     stems = [path.stem for path in paths]
 
     return dict(zip(stems, paths))
+
+def record_fields_from_fit(fit_file):
+    with fd.FitReader(fit_file) as fit:
+        simplified_ride = []
+        simplified_frame = {}
+
+        for frame in fit:
+            # The yielded frame object is of one of the following types:
+            # * fd.FitHeader
+            # * fd.FitDefinitionMessage
+            # * fd.FitDataMessage
+            # * fd.FitCRC
+
+            if isinstance(frame, fd.FitDataMessage) and frame.name == 'record':
+                # Here, frame is a FitDataMessage object.
+                # A FitDataMessage object contains decoded values that
+                # are directly usable in your script logic.
+                for field in frame.fields:
+                    simplified_frame[field.name] = {
+                        'value': field.value,
+                        'raw_value': field.raw_value,
+                        'units': field.units
+                    }
+                simplified_ride.append(simplified_frame)
+                simplified_frame = {}
+    return simplified_ride
     
+def df_from_simplfied_ride(simplified_ride):
+    # Get the available keys from an element that's not near the beginning of the list since the first several records have limited fields for some rides
+    fields = list(simplified_ride[30].keys())
+    units = []
+    for field in fields:
+        units.append(simplified_ride[30][field]['units'])
+    header = [fields, units]
+    
+    field_values = {}
+    for field in fields:
+        values = []
+        for frame in simplified_ride:
+            if field in list(frame.keys()):
+                values.append(frame[field]['value'])
+            else:
+                values.append(None)
+        field_values[field] = values
+
+    df = pd.DataFrame.from_dict(field_values)
+    # Add a second header row for units
+    df.columns = header
+    
+    return df
 
 # Workout Analysis
 
@@ -46,7 +95,21 @@ with title_col1:
 with title_col2:
     st_lottie(st_lottie(lottie_json, height=75))
 
-# f = st.file_uploader('Upload a .fit File', type=None, accept_multiple_files=False, key=None)
+f = st.file_uploader('Upload a .fit File', type=None, accept_multiple_files=False, key=None)
+
+simplified_ride = None
+if f:
+    simplified_ride = record_fields_from_fit(f)
+    
+if simplified_ride:
+    simplified_ride_df = df_from_simplfied_ride(simplified_ride)
+    st.write(simplified_ride_df.head())
+
+
+
+
+
+
 
 fit_dict = dict_for_file_type(Path.cwd(), '.fit')
 selected_fit_dict_keys = st.multiselect('Select .fit Files to Convert to JSON', list(fit_dict.keys()))
